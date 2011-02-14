d84a9c9c7d83d97f948469fdc5b0dd1255f75849
Scott Watson <scott@disney.com>
2011-02-14 07:49:44 -0800

    Parametric window size

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,14 @@
 #include "testApp.h"
 #include "ofAppGlutWindow.h"
 
-//========================================================================
 int main( ){
 
     ofAppGlutWindow window;
-	ofSetupOpenGL(&window, 300,300, OF_WINDOW);			// <-------- setup the GL context
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
+    // for output echo of screen capture area
+	ofSetupOpenGL(&window, INIT_W,INIT_H, OF_WINDOW);			// <-------- setup the GL context
+
+
 	// pass in width and height too:
 	ofRunApp( new testApp());
-
 }
