d1c288c26b6f379d9cf049cb4fd2b5ce332382e7
Scott Watson <scott@disney.com>
2011-02-15 09:51:08 -0800

    Using Blob method for sending now...

--- src/testApp.h
+++ src/testApp.h
@@ -4,8 +4,19 @@
 
 #include "ofMain.h"
 
-#define INIT_H 90
-#define INIT_W 300
+#define OF_ADDON_USING_OFXOSC
+#include "ofAddons.h"
+
+#define INIT_H 45
+#define INIT_W 150
+
+#define OUT_H INIT_H		// NOTE: OUTPUT can not be more than 16384 (64kb) 
+#define OUT_W INIT_W		// due to OSC library limitation
+
+
+// #define OUT_HOST "127.0.0.1"
+#define OUT_HOST "239.192.192.192"	// multicast
+#define OUT_PORT 9192
 
 class testApp : public ofBaseApp{
 
@@ -22,18 +33,24 @@ class testApp : public ofBaseApp{
 		void mouseReleased(int x, int y, int button);
 		void windowResized(int w, int h);
 
-        int capW, capH;
+        int capW, capH;		// capture width and height in pixels
+
+        // OSC
+		ofxOscReceiver	receiver;
+		ofxOscSender	sender;
+        void sendImage(uint* data, int w, int h);
+
+        // display texture
+        ofTexture	tex;
 
         // audio "forced I/O" hack
         void audioReceived 	(float * input, int bufferSize, int nChannels);
         int psnFlag;
         ProcessSerialNumber psn;
 
-        // misc
+        // misc debug
 		int 	bufferCounter, drawCounter, updateCounter;
 
-        // display texture
-        ofTexture	tex;
 };
 
 #endif
