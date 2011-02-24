3eab7af87024a08d9239276a1c79996bd3dbe9d6
Scott Watson <scott@disney.com>
2011-02-23 16:40:28 -0800

    misc

--- testclient.py
+++ testclient.py
@@ -2,7 +2,6 @@
 # Helpers
 ###############################################################################
 
-db = {}
 def ccoreDump(msg):
     # dump a CommuniCore message to stdout and remember the last value we saw
     # in an in memory database
@@ -15,6 +14,17 @@ def ccoreDump(msg):
 # Listen for OSC packets
 
 import CCore
+# MC listen
 tcore = CCore.CCore(pubsub="osc-udp")
 tcore.subscribe("*", ccoreDump)	# catch all 
+
+# Unicast list
+ttcore = CCore.CCore(pubsub="osc-udp://198.178.187.11:9999")
+ttcore.subscribe("*", ccoreDump)
+
+# Unicast list
+# define OUT_HOST "198.178.187.122"
+# define OUT_PORT 9999
+uucore = CCore.CCore(pubsub="osc-udp://198.178.187.122:9999")
+uucore.subscribe("*", ccoreDump)
 
